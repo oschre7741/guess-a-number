58251fc73819fe3d7543d23cc00e1246d7eac509
Schreiner <oschre7741@greenville.k12.sc.us>
2017-09-15 09:07:20 -0400

    added .isnumeric function to recognize if user puts in numbers

--- guess_a_number.py
+++ guess_a_number.py
@@ -12,11 +12,21 @@ print("I'm thinking of a number from " + str(low) + " to " + str(high) + ".");
 guess = -1
 tries = 0
 
+#helper functions
+def get_guess():
+    while True:
+        g = input("Take a guess:")
+
+        if g.isnumeric():
+            g = int(g)
+            return g
+        else:
+            print("You mut enter a number.")
+
 #play the game
 while guess != rand and tries < limit:
-    guess = input("Take a guess: ")
-    guess = int(guess)
-    
+    guess = get_guess()
+   
     if guess < rand:
         print("You guessed too low.")
     elif guess > rand:
