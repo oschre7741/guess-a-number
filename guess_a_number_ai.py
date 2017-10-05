c83d4374c7d96c278849c8daf87d3e7a86ee0815
Schreiner <oschre7741@greenville.k12.sc.us>
2017-10-05 09:32:16 -0400

    added name feature, limit feature, started user input for high and low

--- guess_a_number_ai.py
+++ guess_a_number_ai.py
@@ -5,9 +5,12 @@
 
 #Olivia S.
 
+import math
+
 #config
-low = 1
-high = 100
+low = input()
+high = input()
+limit = math.ceil(math.log(high - low + 1 ,2))
 
 # helper functions
 def show_start_screen():
@@ -33,19 +36,31 @@ def show_credits():
     print("  |_| |_||_|_/__/    \___\__,_|_|_|_|\___|    \_/\_/ \__,_/__/ |_|  |_\__,_\__,_\___|   |___/\_, |  \___/|_||_|\_/ |_\__,_| |___/  ")
     print("                                                                                             |__/                                  ")                                                                                                     
 
-
+def low_and_high():
+    print("What would you like to be the low value?")
+    low = input()
+    return low
+    print("And what would you like to be the high value?")
+    high = input()
+    return high
     
 def get_guess(current_low, current_high):
     guess = (current_high + current_low) // 2
     print(guess)
     return guess
 
-def pick_number():
-    print("Think of a number between " + str(low) + " and " + str(high) + ".")
+def get_name():
+    print("What is your name?")
+    name = input()
+    return name
+
+def pick_number(name, low, high):
+    print("Think of a number between " + str(low) + " and " + str(high) + ", " + str(name))
+    print("I will try to guess your number in " + str(limit) + " tries")
     input("Press enter to continue")
                                    
-def check_guess(guess):
-    print("Was my guess too high, too low, or correct?")
+def check_guess(guess, name):
+    print(str(name) + ", was my guess too high, too low, or correct?")
     answer = input()
     if answer.lower() == "too high" or answer.lower() == "l":
         return 1
@@ -54,10 +69,10 @@ def check_guess(guess):
     elif answer.lower() == "correct" or answer.lower() == "c":
         return 0
     else:
-        print("I don't understand. Please enter too high, too low, or correct.")
+        print("I don't understand, " + str(name) + "." + " Please enter too high, too low, or correct.")
 
-def show_result():
-    print("Haha! I got it!")
+def show_result(name):
+    print("Haha! I got it, " + str(name) + "!")
 
 def play_again():
     while True:
@@ -74,19 +89,24 @@ def play():
     current_low = low
     current_high = high
     check = -1
+    tries = 0
     
-    pick_number()
+    low_and_high()
+    name = get_name()
+    pick_number(name, low, high)
     
     while check != 0:
         guess = get_guess(current_low, current_high)
-        check = check_guess(guess)
+        check = check_guess(guess, name)
+
+        tries += 1
 
         if check == -1:
             current_low = guess
         elif check == 1:
             current_high = guess
            
-    show_result()
+    show_result(name)
 
 # Game starts running here
 show_start_screen()
